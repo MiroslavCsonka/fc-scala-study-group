fc75d48900ed22899fb9f79e92bef49390e23dea
Miroslav Csonka <miroslav.csonka@gmail.com>
2018-03-09 13:16:26 +0000

    WIP since my battery is dying

--- number_guessing.scala
+++ number_guessing.scala
@@ -2,21 +2,49 @@ import scala.io.StdIn._
 import scala.util.{Failure, Success, Try}
 import scala.util.control.Breaks._
 
-def generateValue = (Math.random() * 10).round
+def generateValue: Int = (Math.random() * 10).round.toInt
 
 def readNumber: Try[Int] = Try(readLine().toInt)
 
-println("Guess a number: ")
-
-val actual = generateValue
-
-while (true) {
-  readNumber match {
-    case Success(n) if n == actual =>
-      println("Nailed it")
-      break
-    case Success(_) => println("Try again")
-    case Failure(_) => "You didn't specify a correct number, dumbass"
+def readGameType: Int = {
+  var gameType = 0
+  while (!List(1, 2, 3).contains(gameType) ) {
+    gameType = readNumber.get
   }
+  gameType
+//
+//    // TODO: get rid of .get
+//    if (List(1, 2, 3).contains(gameType.get)) {
+//      error(gameType.get)
+//    }
+//  }
 }
 
+def promptForValue = println("Guess a number: ")
+
+println("Select a game:")
+println("1) guess the number")
+println("2) guess the animal")
+println("3) guess the color")
+
+val gameType = readGameType
+
+if (gameType == 1) {
+  promptForValue
+
+  val actual = generateValue
+
+  while (true) {
+
+    readNumber match {
+      case Success(n) if n == actual =>
+        println("Nailed it")
+        break
+      case Success(_) => println("Try again")
+      case Failure(_) => "You didn't specify a correct number, dumbass"
+    }
+  }
+
+} else {
+  println("Unsupported game type")
+}
